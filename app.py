5c817c2ccfc87ccd8af71106559229bffeaa684d
dee_mavr <mavrdiou.d@outlook.com>
2021-07-19 16:46:17 +0100

    updated app.py

--- app.py
+++ app.py
@@ -1,25 +1,10 @@
-from flask import Flask, render_template, request, send_file
-from whitenoise import WhiteNoise
-import json
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
-app.wsgi_app = WhiteNoise(app.wsgi_app, root='static/', prefix='assets/')
 
 @app.route('/')
-def hello_world():
+def render_route_html():
     return render_template("index.html")
-'''
-@app.route('/get_data')
-def load_data():
-    if request.remote_addr == "":
-        f = open("gods.json")
-        data = json.load(f)
-        f.close()
-        return data
-    else:
-        return "Nothing to see here."
-'''
 
 if __name__ == "__main__":
     app.run()
